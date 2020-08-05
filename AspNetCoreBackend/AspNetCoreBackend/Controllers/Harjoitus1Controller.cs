07d44675470379263edbe8a9480cc4573be3c4fd
Niko Ingraeus <niko.ingraeus@gmail.com>
2020-08-05 14:34:17 +0300

    Lisätty Harjoitus1 tehtävä.

---
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/Harjoitus1Controller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreBackend.Controllers
+{
+    [Route("api/Harjoitus1")]
+    [ApiController]
+    public class Harjoitus1Controller : ControllerBase
+    {
+        public int Luku()
+        {
+            return 123;
+        }   
+    }
+}
