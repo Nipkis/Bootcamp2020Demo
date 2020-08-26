9476e1a62782ea86973725b6433bb6de6a1df726
Niko Ingraeus <niko.ingraeus@gmail.com>
2020-08-26 08:39:39 +0300

    Ensimmäinen versio muistipelistä.

---
+++ FFXIV_Memory_Game/FFXIV_Memory_Game/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FFXIV_Memory_Game
+{
+    public partial class Form1 : Form
+    {
+        bool allowClick = false;
+        PictureBox firstGuess;
+        Random rnd = new Random();
+        Timer clickTimer = new Timer();
+        int time = 60;
+        Timer timer = new Timer { Interval = 1000 };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private PictureBox[] pictureBoxes
+        {
+            get { return Controls.OfType<PictureBox>().ToArray();}
+        }
+        private static IEnumerable<Image> images
+        {
+            get
+            {
+                return new Image[]
+                {
+                    Properties.Resources.Astrologian,
+                    Properties.Resources.Bard,
+                    Properties.Resources.BlackMage,
+                    Properties.Resources.BlueMage,
+                    Properties.Resources.Dancer,
+                    Properties.Resources.DarkKnight,
+                    Properties.Resources.Dragoon,
+                    Properties.Resources.Gunbreaker,
+                    Properties.Resources.Machinist,
+                    Properties.Resources.Monk,
+                    Properties.Resources.Ninja,
+                    Properties.Resources.Paladin,
+                    Properties.Resources.RedMage,
+                    Properties.Resources.Samurai,
+                    Properties.Resources.Scholar,
+                    Properties.Resources.Summoner,
+                    Properties.Resources.Warrior,
+                    Properties.Resources.WhiteMage
+                };
+            }
+        }
+        private void startGameTimer()
+        {
+            timer.Start();
+            timer.Tick += delegate
+            {
+                time--;
+                if (time < 0)
+                {
+                    timer.Stop();
+                    MessageBox.Show("Aika päättyi!");
+                    ResetImages();
+                }
+                var ssTime = TimeSpan.FromSeconds(time);
+
+                label1.Text = "00: " + time.ToString();
+            };
+        }
+        private void ResetImages()
+        {
+            foreach (var pic in pictureBoxes)
+            {
+                pic.Tag = null;
+                pic.Visible = true;
+            }
+            HideImages();
+            setRandomImages();
+            time = 60;
+            timer.Start();
+        }
+        private void HideImages()
+        {
+            foreach (var pic in pictureBoxes)
+            {
+                pic.Image = Properties.Resources.Backside;
+            }
+        }
+        private PictureBox getFreeSlot()
+        {
+            int num;
+            do
+            {
+                num = rnd.Next(0, pictureBoxes.Count());
+            }
+            while (pictureBoxes[num].Tag != null);
+            return pictureBoxes[num];
+        }
+        private void setRandomImages()
+        {
+            foreach (var image in images)
+            {
+                getFreeSlot().Tag = image;
+                getFreeSlot().Tag = image;
+            }
+        }
+        private void CLICKTIMER_TICK(object sender, EventArgs e)
+        {
+            HideImages();
+
+            allowClick = true;
+            clickTimer.Stop();
+        }
+
+        private void clickImage(object sender, EventArgs e)
+        {
+            if (!allowClick) return;
+            var pic = (PictureBox)sender;
+
+            if (firstGuess == null)
+            {
+                firstGuess = pic;
+                pic.Image = (Image)pic.Tag;
+                return;
+            }
+            pic.Image = (Image)pic.Tag;
+
+            if (pic.Image == firstGuess.Image && pic != firstGuess)
+            {
+                pic.Visible = firstGuess.Visible = false;
+                {
+                    firstGuess = pic;
+                }
+            }
+            else
+            {
+                allowClick = false;
+                clickTimer.Start();
+            }
+            firstGuess = null;
+            if (pictureBoxes.Any(p => p.Visible)) return;
+            MessageBox.Show("Onneksi olkoon! Löysit kaikki parit!");
+            ResetImages();
+        }
+
+        private void startGame(object sender, EventArgs e)
+        {
+            allowClick = true;
+            setRandomImages();
+            HideImages();
+            startGameTimer();
+            clickTimer.Interval = 1000;
+            clickTimer.Tick += CLICKTIMER_TICK;
+            button1.Enabled = false;
+        }
+    }
+}
